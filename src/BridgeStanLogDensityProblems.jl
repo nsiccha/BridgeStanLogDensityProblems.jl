d8885ef0ca08020655e7ef57f77411ea9c02102d
Nikolas Siccha <nikolassiccha@gmail.com>
2023-08-16 09:30:06 +0300

    autocommit

--- src/BridgeStanLogDensityProblems.jl
+++ src/BridgeStanLogDensityProblems.jl
@@ -16,7 +16,11 @@ BridgeStanLogDensityProblem(stan_file::AbstractString, data::AbstractString; kwa
 LogDensityProblems.dimension(what::BridgeStanLogDensityProblem) = BridgeStan.param_unc_num(what.posterior)
 LogDensityProblems.capabilities(::Type{<:BridgeStanLogDensityProblem}) = LogDensityProblems.LogDensityOrder{2}()
 LogDensityProblems.logdensity(what::BridgeStanLogDensityProblem, x) = BridgeStan.log_density(what.posterior, x)
-LogDensityProblems.logdensity_and_gradient(what::BridgeStanLogDensityProblem, x) = BridgeStan.log_density_gradient(what.posterior, x)
+LogDensityProblems.logdensity_and_gradient(what::BridgeStanLogDensityProblem, x) = try 
+    BridgeStan.log_density_gradient(what.posterior, x)
+catch e
+    -Inf, -Inf .* x
+end
 LogDensityProblems.logdensity_gradient_and_hessian(what::BridgeStanLogDensityProblem, x) = BridgeStan.log_density_hessian(what.posterior, x)
 
 
